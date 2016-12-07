e5c239d6251fa722368acb77c7616fbe4a67661b
Douglas Q. dos Santos <douglas.q.santos@gmail.com>
2016-12-07 09:31:03 -0200

    Importing the pycrackzip

---
+++ Crackers/pycrackzip/README.md
@@ -0,0 +1,16 @@
+# pycrackzip
+Zip Files Cracker
+
+## Examples of usage
+
+Here in the repo I appended th rockyou wordlist so you can test the pycrackzip using that wordlist or you can use another one with your pattern.
+
+```bash
+python pycrackzip-no-thread.py -f expressions.zip -d rockyou.txt
+```
+
+## Task Lists
+- [ ] The pycrackzip.py script file has one issue that we are working on to fix, when start the pycrackzip.py the script found the password but it does not stop after the password is found.
+
+# TEAM
+- [pogTeam](https://github.com/pogTeam/)
---
+++ Crackers/pycrackzip/pycrackzip-no-thread.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# By: E4404_404
+# pogTeam
+# Date: 07/12/2016
+
+# https://docs.python.org/2/library/zipfile.html?highlight=zipfile#module-zipfile
+import zipfile
+# https://docs.python.org/2/library/optparse.html?highlight=optparse#module-optparse
+# https://docs.python.org/2/library/argparse.html#module-argparse
+import optparse
+# https://docs.python.org/2/library/threading.html
+from threading import *
+# https://pypi.python.org/pypi/termcolor
+from termcolor import colored
+
+
+def banner():
+    print '**************************************************'
+    print 'PyCrackZip by: E4404_404'
+    print 'pogTeam: https://github.com/pogTeam/'
+    print '**************************************************\n'
+
+
+def usage():
+    print '**************************************************'
+    print 'Usage: '
+    print ' -f: specify zip file'
+    print ' -d: specify dictionary file\n'
+    print 'example: ./pyzip.py -f file.zip -d wordlist \n'
+    print '**************************************************'
+
+def extractFile(zFile, password):
+    try:
+        zFile.extractall(pwd=password)
+        print '[+] Found password: ' + colored(password,'green') + '\n'
+        return "Found"
+    except:
+        pass
+
+def main():
+    banner()
+    # parser = optparse.OptionParser("usage: ./pyzip.py " + "-f file.zip -d wordlist")
+    parser.add_option('-f', dest='zname', type='string',help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string',help='specify dictionary file')
+
+    (options, args) = parser.parse_args()
+
+    if (options.zname == None) | (options.dname == None):
+        usage()
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+        zFile = zipfile.ZipFile(zname)
+        passFile = open(dname)
+
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        test = extractFile(zFile, password)
+        if(str(test) == "Found"):
+            exit(0)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Check pyzip interrupted by user, killing..!!"
